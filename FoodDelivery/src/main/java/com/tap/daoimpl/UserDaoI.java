20ff16e3d41a70f9b90be6f231e13bda3853e6d9
Ashish Gunaki <138620846+ashishg1212@users.noreply.github.com>
2024-07-02 13:39:24 +0530

    Initial Commit

--- FoodDelivery/src/main/java/com/tap/daoimpl/UserDaoI.java
+++ FoodDelivery/src/main/java/com/tap/daoimpl/UserDaoI.java
@@ -26,9 +26,9 @@ public class UserDaoI implements UserDao {
 
     // Constructor to establish database connection
     public UserDaoI() {
-    	String url = "jdbc:mysql://localhost:3306/tapfoods";
-        String username = "root";
-        String password = "root";
+    	String url = "jdbc:mysql://localhost:3306/your_databse_name";
+        String username = "your_username";
+        String password = "your_password";
 
         try {
         	Class.forName("com.mysql.cj.jdbc.Driver");
